8a05b41de5e68ed539b452bbdd303c1aeced6270
druid <angular.druid@gmail.com>
2022-09-27 16:19:08 -0400

    [tz-32] Begin adding project pages.

--- projects/spear/src/environments/panelpages.ts
+++ projects/spear/src/environments/panelpages.ts
@@ -1,4 +1,5 @@
 // Take note args used in demo paths can not be included here.
 export const panelpages = [
+  ['b43dcd61-d6c9-4ae6-8c10-706e64a0e842', '/projects/sustainable-cloud'],
   ['f4c36261-0886-4a6a-8883-7ccf530da319', '/']
 ];
